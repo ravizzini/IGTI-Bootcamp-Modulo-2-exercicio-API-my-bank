8b53080158794523a52ad266fc37b207130597d2
Victor Coelho <victor.ravizzini@gmail.com>
2020-07-15 18:03:21 -0300

    promises com async await aplicado em todo projeto

--- index.js
+++ index.js
@@ -8,24 +8,23 @@ global.fileName = 'accounts.json'; //criação de variavel global
 app.use(express.json());
 app.use('/account', accountsRouter); //informa que deve utilizar o router
 
-app.listen(3000, function () {
+app.listen(3000, async () => {
   //verificar se existe objeto json se não existir criar json com a estrutura que definirmos. Melhor lugar para verificar é quando a api sobe.
+
+  //try-catch com async await try tenta ler o arquivo com readFile se não conseguir cair no catch(error) e cria um novo arquivo
   try {
-    fs.readFile(global.fileName, 'utf8').catch(() => {
-      const initialJson = {
-        nextId: 1, //nextId cria id incrementado
-        accounts: [],
-      };
-
-      fs.writeFile(global.fileName, JSON.stringify(initialJson)).catch(
-        (error) => {
-          console.log(error);
-        }
-      );
-    });
+    await fs.readFile(global.fileName, 'utf8');
+    console.log('API Started');
   } catch (error) {
-    console.log(error);
-  }
+    const initialJson = {
+      nextId: 1, //nextId cria id incrementado
+      accounts: [],
+    };
 
-  console.log('API Started');
+    fs.writeFile(global.fileName, JSON.stringify(initialJson)).catch(
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 });
--- routes/accounts.js
+++ routes/accounts.js
@@ -1,190 +1,149 @@
 var express = require('express');
-var fs = require('fs');
+var fs = require('fs').promises;
 
 var router = express.Router(); //cria objeto router para substituir app uma vez que todos endpoint respondem na mesma url
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   //pegar parametros que estão sendo enviados
   let account = req.body;
-  //gravação em arquivo vamos usar o filesystem
-  //writeFile cria o arquivo. Como ele espera uma string usamos o método stringify para transformar de JSON para string ou parse para transformar de string para json
-  //writeFile recebe 3 parametros ('nome do arquivo', string, função callback erro )
-  //o metodo write file cria um novo arquivp e substitui o anterior.
-  // Melhor usar o metodo appendFile que cria o primeiro e adiciona os demais.
-  // para criar um objeto json corretamente devemo primeiro ler o que foi criado com o metodo readFile para adicionar ao array
-  //readFile recebe 3 parametos: nome do arquivo, codificação "utf8", callback
 
-  fs.readFile(global.fileName, 'utf8', (error, data) => {
-    //verificar se não houver erro executar
+  try {
+    //data da callback passa a ser retornado pela promisse caso ela tenha sucesso. Erro retornado no catch
+    let data = await fs.readFile(global.fileName, 'utf8');
 
-    //para manipular o arquivo temos que transformar a string em json usando JSON.parse
-    //usar bloco try-catch para tratamento de erro em caso do arquivo json estar corrompido
-    try {
-      if (error) throw error;
+    let json = JSON.parse(data); // le o arquivo
+    //console.log(json);
 
-      let json = JSON.parse(data); // le o arquivo
-      //console.log(json);
-      // operador ... destructing faz a mesma função que criar as propiedades do objeto name: account.name, balance: account.balance
-      account = { id: json.nextId++, ...account }; //constroi o objeto account
+    // operador ... destructing faz a mesma função que criar as propiedades do objeto name: account.name, balance: account.balance
+    account = { id: json.nextId++, ...account }; //constroi o objeto account
+    json.accounts.push(account); // inserindo objeto no final do array account
 
-      json.accounts.push(account); // inserindo objeto no final do array account
+    //escrita do conteudo. Como já foi lido podemos usar o writeFile e reescrever o novo arquivo
+    await fs.writeFile(global.fileName, JSON.stringify(json));
 
-      //escrita do conteudo. Como já foi lido podemos usar o writeFile e reescrever o novo arquivo
-      fs.writeFile(global.fileName, JSON.stringify(json), (error) => {
-        if (error) {
-          res.status(400).send({ error: error.message });
-        } else {
-          //res.send('post account');
-          res.end(); // retorna status 200
-        }
-      });
-    } catch (error) {
-      //res.send('erro');
-      res.status(400).send({ error: error.message });
-    }
-  });
+    //res.end(); // retorna status 200
+    res.status(200).send('Conta cadastrada com sucesso');
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
 });
 
-router.get('/', (_, res) => {
-  //ler o arquivo
-  fs.readFile(global.fileName, 'utf8', (error, data) => {
-    try {
-      if (error) throw error;
+router.get('/', async (_, res) => {
+  try {
+    //ler o arquivo
+    let data = await fs.readFile(global.fileName, 'utf8');
 
-      // criando variavel json para responder data usando a conversão parse e remover o next id com delete json.nextID
-      let json = JSON.parse(data);
-      delete json.nextId;
-      res.send(json);
-    } catch (error) {
-      res.status(400).send({ error: error.message });
-    }
-  });
+    // criando variavel json para responder data usando a conversão parse e remover o next id com delete json.nextID
+    let json = JSON.parse(data);
+    delete json.nextId;
+    res.send(json);
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
 });
 
-router.get('/:id', (req, res) => {
-  fs.readFile(global.fileName, 'utf8', (error, data) => {
-    try {
-      if (error) {
-        throw error;
-      }
+router.get('/:id', async (req, res) => {
+  try {
+    let data = await fs.readFile(global.fileName, 'utf8');
 
-      // criando variavel json para responder data usando a conversão parse e remover o next id com delete json.nextID
-      let json = JSON.parse(data);
-      const account = json.accounts.find(
-        (account) => account.id === parseInt(req.params.id, 10) // parseInt usado para converter string para inteiro
-      ); //usar o metodo find para localizar o objeto com id fornecido no array accounts
+    // criando variavel json para responder data usando a conversão parse e remover o next id com delete json.nextID
+    let json = JSON.parse(data);
+    const account = json.accounts.find(
+      (account) => account.id === parseInt(req.params.id, 10) // parseInt usado para converter string para inteiro
+    ); //usar o metodo find para localizar o objeto com id fornecido no array accounts
 
-      if (account) {
-        res.send(account);
-      } else {
-        res.status(200).send('Conta não localizada');
-      }
-    } catch (error) {
-      res.status(400).send({ error: error.message });
+    if (account) {
+      res.send(account);
+    } else {
+      res.status(200).send('Conta não localizada');
     }
-  });
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  fs.readFile(global.fileName, 'utf8', (error, data) => {
-    try {
-      if (error) {
-        throw error;
-      }
+router.delete('/:id', async (req, res) => {
+  try {
+    let data = await fs.readFile(global.fileName, 'utf8');
 
-      // criando variavel json para responder data usando a conversão parse e remover o next id com delete json.nextID
+    // criando variavel json para responder data usando a conversão parse e remover o next id com delete json.nextID
 
-      let json = JSON.parse(data);
-      //é possivel inserir um tratamento de erro aqui caso não exista o id no array
-      let accounts = json.accounts.filter(
-        (account) => account.id !== parseInt(req.params.id, 10) // parseInt usado para converter string para inteiro
-      ); //usar o metodo filter para localizar o objeto com id fornecido no array accounts e remover retornando o array sem o objeto
-      json.accounts = accounts; // troca o array com todos os registros pelo novo array
-      //escreve o novo arquivo
-      fs.writeFile(global.fileName, JSON.stringify(json), (error) => {
-        if (error) {
-          res.status(400).send({ error: error.message });
-        } else {
-          res.end(); // retorna status 200
-        }
-      });
-    } catch (error) {
-      res.status(400).send({ error: error.message });
-    }
-  });
+    let json = JSON.parse(data);
+    //é possivel inserir um tratamento de erro aqui caso não exista o id no array
+
+    let accounts = json.accounts.filter(
+      (account) => account.id !== parseInt(req.params.id, 10) // parseInt usado para converter string para inteiro
+    ); //usar o metodo filter para localizar o objeto com id fornecido no array accounts e remover retornando o array sem o objeto
+
+    json.accounts = accounts; // troca o array com todos os registros pelo novo array
+
+    //escreve o novo arquivo
+
+    await fs.writeFile(global.fileName, JSON.stringify(json));
+
+    //res.end(); // retorna status 200
+    res.status(200).send('Conta removida com sucesso');
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
 });
 
 //put utilizado para atualizar completamente o registro. Patch para atualização parcial
 
-router.put('/', (req, res) => {
-  //pegar parametros que estão sendo enviados
-  let newAccount = req.body;
+router.put('/', async (req, res) => {
+  try {
+    //pegar parametros que estão sendo enviados
+    let newAccount = req.body;
 
-  fs.readFile(global.fileName, 'utf8', (error, data) => {
-    try {
-      if (error) throw error;
+    let data = await fs.readFile(global.fileName, 'utf8');
 
-      let json = JSON.parse(data); //Lê a informação do arquivo
+    let json = JSON.parse(data); //Lê a informação do arquivo
 
-      let oldIndex = json.accounts.findIndex(
-        (account) => account.id === newAccount.id
-      ); //método dinfIndex encontra o índice anterior ao registro que desejamos alterar
+    let oldIndex = json.accounts.findIndex(
+      (account) => account.id === newAccount.id
+    ); //método dinfIndex encontra o índice anterior ao registro que desejamos alterar
 
-      json.accounts[oldIndex].name = newAccount.name; //altera a posição do registro com os valores recebidos da requisição
-      json.accounts[oldIndex].balance = newAccount.balance;
+    json.accounts[oldIndex].name = newAccount.name; //altera a posição do registro com os valores recebidos da requisição
+    json.accounts[oldIndex].balance = newAccount.balance;
 
-      fs.writeFile(global.fileName, JSON.stringify(json), (error) => {
-        if (error) {
-          res.status(400).send({ error: error.message });
-        } else {
-          res.status(200).send('Conta atualizada');
-          //res.end(); // retorna status 200
-        }
-      });
-    } catch (error) {
-      res.status(400).send({ error: error.message });
-    }
-  });
+    await fs.writeFile(global.fileName, JSON.stringify(json));
+
+    res.status(200).send('Conta atualizada');
+    //res.end(); // retorna status 200
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
 });
 
-router.post('/transaction', (req, res) => {
+router.post('/transaction', async (req, res) => {
   //pegar parametros que estão sendo enviados
-  let params = req.body;
 
-  fs.readFile(global.fileName, 'utf8', (error, data) => {
-    try {
-      if (error) throw error;
+  try {
+    let params = req.body;
 
-      // let json = JSON.parse(data); //Lê a informação do arquivo
+    let data = await fs.readFile(global.fileName, 'utf8');
 
-      let json = JSON.parse(data);
-      let index = json.accounts.findIndex(
-        (account) => account.id === params.id
-      );
+    // let json = JSON.parse(data); //Lê a informação do arquivo
 
-      // let index = json.accounts.findIndex(
-      //   (account) => account.id === params.id
-      // ); //método dinfIndex encontra o índice anterior ao registro que desejamos alterar
+    let json = JSON.parse(data);
+    let index = json.accounts.findIndex((account) => account.id === params.id);
 
-      // prettier-ignore
-      if ((params.value < 0) && ((json.accounts[index].balance + params.value) < 0)) {
+    //método dinfIndex encontra o índice anterior ao registro que desejamos alterar
+
+    // prettier-ignore
+    if ((params.value < 0) && ((json.accounts[index].balance + params.value) < 0)) {
         throw new Error("Não há saldo suficiente.");
       }
 
-      json.accounts[index].balance += params.value;
+    json.accounts[index].balance += params.value;
 
-      fs.writeFile(global.fileName, JSON.stringify(json), (error) => {
-        if (error) {
-          res.status(400).send({ error: error.message });
-        } else {
-          res.send(json.accounts[index]);
-          //res.end(); // retorna status 200
-        }
-      });
-    } catch (error) {
-      res.status(400).send({ error: error.message });
-    }
-  });
+    await fs.writeFile(global.fileName, JSON.stringify(json));
+
+    res.send(json.accounts[index]);
+    //res.end(); // retorna status 200
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
 });
 
 module.exports = router;
